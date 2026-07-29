69213d62daad06f00e9cb68e2f03ecbc23037d4e
Shahymee <mmsshaheem@gmail.com>
2026-07-29 21:16:06 +0800

    game.js

--- game.js
+++ game.js
@@ -2,76 +2,255 @@ let club = "";
 let squad = [];
 
 const players = [
-    "Lionel Messi",
-    "Cristiano Ronaldo",
-    "Kylian Mbappe",
-    "Erling Haaland",
-    "Vinicius Jr",
-    "Jude Bellingham",
-    "Kevin De Bruyne",
-    "Rodri",
-    "Pedri",
-    "Virgil van Dijk",
-    "William Saliba",
-    "Alisson",
-    "Thibaut Courtois"
+
+{
+name:"Erling Haaland",
+club:"Manchester City",
+league:"Premier League",
+position:"ST",
+rating:91
+},
+
+{
+name:"Kylian Mbappe",
+club:"Real Madrid",
+league:"La Liga",
+position:"LW/ST",
+rating:91
+},
+
+{
+name:"Vinicius Jr",
+club:"Real Madrid",
+league:"La Liga",
+position:"LW",
+rating:90
+},
+
+{
+name:"Jude Bellingham",
+club:"Real Madrid",
+league:"La Liga",
+position:"CM",
+rating:90
+},
+
+{
+name:"Rodri",
+club:"Manchester City",
+league:"Premier League",
+position:"CDM",
+rating:90
+},
+
+{
+name:"Kevin De Bruyne",
+club:"Manchester City",
+league:"Premier League",
+position:"CAM",
+rating:88
+},
+
+{
+name:"Mohamed Salah",
+club:"Liverpool",
+league:"Premier League",
+position:"RW",
+rating:88
+},
+
+{
+name:"Bukayo Saka",
+club:"Arsenal",
+league:"Premier League",
+position:"RW",
+rating:87
+},
+
+{
+name:"William Saliba",
+club:"Arsenal",
+league:"Premier League",
+position:"CB",
+rating:87
+},
+
+{
+name:"Virgil van Dijk",
+club:"Liverpool",
+league:"Premier League",
+position:"CB",
+rating:86
+},
+
+{
+name:"Florian Wirtz",
+club:"Bayer Leverkusen",
+league:"Bundesliga",
+position:"CAM",
+rating:87
+},
+
+{
+name:"Harry Kane",
+club:"Bayern Munich",
+league:"Bundesliga",
+position:"ST",
+rating:90
+},
+
+{
+name:"Lautaro Martinez",
+club:"Inter Milan",
+league:"Serie A",
+position:"ST",
+rating:88
+},
+
+{
+name:"Victor Osimhen",
+club:"Napoli",
+league:"Serie A",
+position:"ST",
+rating:87
+},
+
+{
+name:"Ousmane Dembele",
+club:"PSG",
+league:"Ligue 1",
+position:"RW",
+rating:86
+},
+
+{
+name:"Achraf Hakimi",
+club:"PSG",
+league:"Ligue 1",
+position:"RB",
+rating:87
+},
+
+{
+name:"Alisson Becker",
+club:"Liverpool",
+league:"Premier League",
+position:"GK",
+rating:89
+},
+
+{
+name:"Thibaut Courtois",
+club:"Real Madrid",
+league:"La Liga",
+position:"GK",
+rating:89
+}
+
 ];
 
 
-window.onload = function(){
+window.onload=function(){
 
-    let list = document.getElementById("playerList");
+let list=document.getElementById("playerList");
 
-    players.forEach(player => {
-        let option = document.createElement("option");
-        option.text = player;
-        list.add(option);
-    });
+players.forEach(player=>{
 
-}
+let option=document.createElement("option");
+
+option.text=
+player.name+
+" ("+
+player.position+
+") - "+
+player.rating;
+
+list.add(option);
+
+});
+
+};
+
 
 
 function startGame(){
 
-    club = document.getElementById("clubName").value;
+club=document.getElementById("clubName").value;
 
-    if(club === ""){
-        alert("Enter a club name!");
-        return;
-    }
+if(club===""){
+alert("Enter club name");
+return;
+}
 
-    document.getElementById("teamBox").style.display = "block";
+document.getElementById("teamBox").style.display="block";
 
-    alert(club + " has been created!");
+alert(club+" created!");
 
 }
+
 
 
 function addPlayer(){
 
-    let player = document.getElementById("playerList").value;
+let selected=document.getElementById("playerList").value;
 
-    if(squad.includes(player)){
-        alert("Player already selected!");
-        return;
-    }
+let player=players.find(
+p=>selected.includes(p.name)
+);
 
-    squad.push(player);
 
-    let li = document.createElement("li");
-    li.innerHTML = player;
+if(squad.includes(player)){
+alert("Already selected!");
+return;
+}
 
-    document.getElementById("squad").appendChild(li);
+
+squad.push(player);
+
+
+let li=document.createElement("li");
+
+li.innerHTML=
+player.name+
+" | "+
+player.position+
+" | Rating "+
+player.rating;
+
+
+document.getElementById("squad").appendChild(li);
 
 }
 
 
+
 function simulateMatch(){
 
-    let yourGoals = Math.floor(Math.random() * 5);
-    let opponentGoals = Math.floor(Math.random() * 5);
+let strength=0;
 
-    document.getElementById("result").innerHTML =
-    club + " " + yourGoals + " - " + opponentGoals + " AI United";
+squad.forEach(player=>{
+strength+=player.rating;
+});
+
+
+let yourGoals=Math.floor(
+Math.random()*5
+);
+
+
+let opponentGoals=Math.floor(
+Math.random()*5
+);
+
+
+document.getElementById("result").innerHTML=
+
+club+
+" "+
+yourGoals+
+" - "+
+opponentGoals+
+" AI United";
+
 
 }
