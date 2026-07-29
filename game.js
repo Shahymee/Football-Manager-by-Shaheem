88d21330006e2cc5a7e87917ca8b3878e2883d11
Shahymee <mmsshaheem@gmail.com>
2026-07-29 21:23:30 +0800

    game.js

--- game.js
+++ game.js
@@ -3,6 +3,8 @@ let squad = [];
 
 const players = [
 
+/* PREMIER LEAGUE */
+
 {
 name:"Erling Haaland",
 club:"Manchester City",
@@ -12,27 +14,11 @@ rating:91
 },
 
 {
-name:"Kylian Mbappe",
-club:"Real Madrid",
-league:"La Liga",
-position:"LW/ST",
-rating:91
-},
-
-{
-name:"Vinicius Jr",
-club:"Real Madrid",
-league:"La Liga",
-position:"LW",
-rating:90
-},
-
-{
-name:"Jude Bellingham",
-club:"Real Madrid",
-league:"La Liga",
-position:"CM",
-rating:90
+name:"Phil Foden",
+club:"Manchester City",
+league:"Premier League",
+position:"LW/CAM",
+rating:85
 },
 
 {
@@ -52,10 +38,34 @@ rating:88
 },
 
 {
-name:"Mohamed Salah",
-club:"Liverpool",
+name:"Bernardo Silva",
+club:"Manchester City",
 league:"Premier League",
-position:"RW",
+position:"CM/RW",
+rating:87
+},
+
+{
+name:"Ruben Dias",
+club:"Manchester City",
+league:"Premier League",
+position:"CB",
+rating:87
+},
+
+{
+name:"Josko Gvardiol",
+club:"Manchester City",
+league:"Premier League",
+position:"CB/LB",
+rating:85
+},
+
+{
+name:"Ederson",
+club:"Manchester City",
+league:"Premier League",
+position:"GK",
 rating:88
 },
 
@@ -68,11 +78,59 @@ rating:87
 },
 
 {
+name:"Martin Odegaard",
+club:"Arsenal",
+league:"Premier League",
+position:"CAM",
+rating:86
+},
+
+{
+name:"Declan Rice",
+club:"Arsenal",
+league:"Premier League",
+position:"CDM/CM",
+rating:86
+},
+
+{
 name:"William Saliba",
 club:"Arsenal",
 league:"Premier League",
 position:"CB",
 rating:87
+},
+
+{
+name:"Gabriel Magalhaes",
+club:"Arsenal",
+league:"Premier League",
+position:"CB",
+rating:85
+},
+
+{
+name:"Gabriel Martinelli",
+club:"Arsenal",
+league:"Premier League",
+position:"LW",
+rating:83
+},
+
+{
+name:"David Raya",
+club:"Arsenal",
+league:"Premier League",
+position:"GK",
+rating:83
+},
+
+{
+name:"Mohamed Salah",
+club:"Liverpool",
+league:"Premier League",
+position:"RW",
+rating:88
 },
 
 {
@@ -84,54 +142,6 @@ rating:86
 },
 
 {
-name:"Florian Wirtz",
-club:"Bayer Leverkusen",
-league:"Bundesliga",
-position:"CAM",
-rating:87
-},
-
-{
-name:"Harry Kane",
-club:"Bayern Munich",
-league:"Bundesliga",
-position:"ST",
-rating:90
-},
-
-{
-name:"Lautaro Martinez",
-club:"Inter Milan",
-league:"Serie A",
-position:"ST",
-rating:88
-},
-
-{
-name:"Victor Osimhen",
-club:"Napoli",
-league:"Serie A",
-position:"ST",
-rating:87
-},
-
-{
-name:"Ousmane Dembele",
-club:"PSG",
-league:"Ligue 1",
-position:"RW",
-rating:86
-},
-
-{
-name:"Achraf Hakimi",
-club:"PSG",
-league:"Ligue 1",
-position:"RB",
-rating:87
-},
-
-{
 name:"Alisson Becker",
 club:"Liverpool",
 league:"Premier League",
@@ -140,14 +150,174 @@ rating:89
 },
 
 {
-name:"Thibaut Courtois",
-club:"Real Madrid",
-league:"La Liga",
+name:"Alexis Mac Allister",
+club:"Liverpool",
+league:"Premier League",
+position:"CM",
+rating:84
+},
+
+{
+name:"Dominik Szoboszlai",
+club:"Liverpool",
+league:"Premier League",
+position:"CM/CAM",
+rating:82
+},
+
+{
+name:"Luis Diaz",
+club:"Liverpool",
+league:"Premier League",
+position:"LW",
+rating:84
+},
+
+{
+name:"Darwin Nunez",
+club:"Liverpool",
+league:"Premier League",
+position:"ST",
+rating:82
+},
+
+{
+name:"Cole Palmer",
+club:"Chelsea",
+league:"Premier League",
+position:"CAM/RW",
+rating:85
+},
+
+{
+name:"Enzo Fernandez",
+club:"Chelsea",
+league:"Premier League",
+position:"CM",
+rating:82
+},
+
+{
+name:"Moises Caicedo",
+club:"Chelsea",
+league:"Premier League",
+position:"CDM",
+rating:81
+},
+
+{
+name:"Christopher Nkunku",
+club:"Chelsea",
+league:"Premier League",
+position:"ST/CAM",
+rating:82
+},
+
+{
+name:"Nicolas Jackson",
+club:"Chelsea",
+league:"Premier League",
+position:"ST",
+rating:79
+},
+
+{
+name:"Bruno Fernandes",
+club:"Manchester United",
+league:"Premier League",
+position:"CAM",
+rating:84
+},
+
+{
+name:"Marcus Rashford",
+club:"Manchester United",
+league:"Premier League",
+position:"LW",
+rating:81
+},
+
+{
+name:"Kobbie Mainoo",
+club:"Manchester United",
+league:"Premier League",
+position:"CM",
+rating:78
+},
+
+{
+name:"Andre Onana",
+club:"Manchester United",
+league:"Premier League",
 position:"GK",
-rating:89
+rating:80
+},
+
+{
+name:"Son Heung-min",
+club:"Tottenham",
+league:"Premier League",
+position:"LW/ST",
+rating:84
+},
+
+{
+name:"James Maddison",
+club:"Tottenham",
+league:"Premier League",
+position:"CAM",
+rating:81
+},
+
+{
+name:"Cristian Romero",
+club:"Tottenham",
+league:"Premier League",
+position:"CB",
+rating:82
+},
+
+{
+name:"Alexander Isak",
+club:"Newcastle United",
+league:"Premier League",
+position:"ST",
+rating:83
+},
+
+{
+name:"Bruno Guimaraes",
+club:"Newcastle United",
+league:"Premier League",
+position:"CM",
+rating:84
+},
+
+{
+name:"Anthony Gordon",
+club:"Newcastle United",
+league:"Premier League",
+position:"LW",
+rating:81
+},
+
+{
+name:"Ollie Watkins",
+club:"Aston Villa",
+league:"Premier League",
+position:"ST",
+rating:82
+},
+
+{
+name:"Douglas Luiz",
+club:"Aston Villa",
+league:"Premier League",
+position:"CM",
+rating:82
 }
 
-];
+];];
 
 
 window.onload=function(){
